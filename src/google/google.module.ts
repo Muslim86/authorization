3e0fb0821319d87e0a9b4fd94c87dfa89ccb34da
Muslim86 <47973991+Muslim86@users.noreply.github.com>
2021-07-02 11:18:57 +0500

    save google users

--- src/google/google.module.ts
+++ src/google/google.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
 import { GoogleController } from './google.controller';
 import { GoogleService } from './google.service';
 import { GoogleStrategy } from './google.strategy';
 
 @Module({
   controllers: [GoogleController],
-  providers: [GoogleService, GoogleStrategy]
+  providers: [GoogleService, GoogleStrategy],
+  imports: [
+    UsersModule
+  ],
+  exports: [
+    GoogleModule
+  ]
 })
 export class GoogleModule {}
--- src/google/google.service.ts
+++ src/google/google.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class GoogleService {
-    googleLogin(req: { user: any; }) {
+
+    constructor(private userService: UsersService) {}
+
+    async googleLogin(req: { user: any; }) {
         if (!req.user) {
             console.log("логин провал")
             return 'Нет пользователя гугл'
+        }
+
+        const user = await this.userService.getUserByEmail(req.user.emails);
+        if (user) {
+            console.log("Такой гугл-пользователь есть")
+        } else {
+            const userDto = {
+                "email": req.user.emails,
+                "password": req.user.accesToken
+            }
+            const newUser = this.userService.createUser(userDto);
         }
         return {
             message: 'Информация о пользователе гугл',
